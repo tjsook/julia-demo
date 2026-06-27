a49215d6be1a5f8b4d2e377044633da0040af752
Tyler Kim <tylerjsook@gmail.com>
2026-06-26 20:05:59 -0700

    Fix NextAuth session endpoint routing

--- frontend/pages/api/submit-ticket.ts
+++ frontend/pages/api/submit-ticket.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./auth/[...nextauth]";
+import { authOptions } from "../../lib/authOptions";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
