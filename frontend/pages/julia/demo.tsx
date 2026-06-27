e2a58ff6ad92bf11d0fd5f36ae8e849c9e7687e2
Tyler Kim <tylerjsook@gmail.com>
2026-06-26 19:03:04 -0700

    Remove sidebar from Julia demo page

--- frontend/pages/julia/demo.tsx
+++ frontend/pages/julia/demo.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
 
-import { DashboardSidebar } from "../../components/dashboard-sidebar";
 import { DemoShell } from "../../components/Julia/Demo/DemoShell";
 import { useJuliaDemo } from "../../hooks/julia/useJuliaDemo";
-import dashboardStyles from "../../styles/dashboard.module.css";
 
 export default function JuliaDemoPage() {
   const demo = useJuliaDemo();
@@ -13,22 +11,19 @@ export default function JuliaDemoPage() {
       <Head>
         <title>Julia Demo | Diesel Dashboard</title>
       </Head>
-      <div className={dashboardStyles.layout}>
-        <DashboardSidebar activeRoute="julia" />
-        <DemoShell
-          state={demo.state}
-          errorToast={demo.errorToast}
-          activeMatch={demo.activeMatch}
-          selectorMatches={demo.selectorMatches}
-          documentUrl={demo.documentUrl}
-          documentLoading={demo.documentLoading}
-          documentError={demo.documentError}
-          onOrbClick={demo.handleOrbClick}
-          onSelectMatch={(match) => void demo.openDocument(match)}
-          onCloseForeground={demo.closeForeground}
-          onDismissError={demo.dismissError}
-        />
-      </div>
+      <DemoShell
+        state={demo.state}
+        errorToast={demo.errorToast}
+        activeMatch={demo.activeMatch}
+        selectorMatches={demo.selectorMatches}
+        documentUrl={demo.documentUrl}
+        documentLoading={demo.documentLoading}
+        documentError={demo.documentError}
+        onOrbClick={demo.handleOrbClick}
+        onSelectMatch={(match) => void demo.openDocument(match)}
+        onCloseForeground={demo.closeForeground}
+        onDismissError={demo.dismissError}
+      />
     </>
   );
 }
